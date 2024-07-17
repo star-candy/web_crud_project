12d8d54c893accee2edb20446bb36e811a306c50
star-candy <el0710@naver.com>
2024-07-17 20:01:45 +0900

    <feat> gradle(4.10.2), spring(2.1.9), java(8) 버전 조정

---
+++ src/test/java/com/curd/test/web_crud_project/WebCrudProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curd.test.web_crud_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebCrudProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
