a8dedaaf45da0dda7d1e95a9041826bc27b7d712
star-candy <el0710@naver.com>
2024-07-21 11:46:21 +0900

    <feat> posts test

---
+++ src/main/java/com/curd/test/project/domain/posts/Posts.java
@@ -0,0 +1,42 @@
+package com.curd.test.project.domain.posts;
+
+//import com.jojoldu.book.springboot.domain.BaseTimeEntity;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter //모든 필드 대한 getter 정의
+@NoArgsConstructor //기본 생성자 정의
+@Entity //jpa annotation, 테이블과 매핑되는 클래스 의미
+public class Posts {
+
+    @Id //class의 pk 필드
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //pk 규칙 정의
+    private Long id;
+
+    @Column(length = 500, nullable = false) //테이블 열 의미(size 500)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    private String author;
+
+    @Builder //class의 빌더, 값 초기화요소 명시하는 생성자
+    public Posts(String title, String content, String author) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+
+    /* void update(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }*/
+}
---
+++ src/test/java/com/curd/test/project/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,68 @@
+package com.curd.test.project.domain.posts;
+
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PostsRepositoryTest {
+
+    @Autowired
+    PostsRepository postsRepository;
+
+    @After //테스트 완료후 초기화 동작
+    public void cleanup() {
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void 게시글저장_불러오기() {
+        //given
+        String title = "테스트 게시글";
+        String content = "테스트 본문";
+
+        postsRepository.save(Posts.builder() //posts에 insert, update 진행
+                .title(title)
+                .content(content)
+                .author("jojoldu@gmail.com")
+                .build());
+
+        //when
+        List<Posts> postsList = postsRepository.findAll(); //posts의 모든 데이터 조회
+
+        //then
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle()).isEqualTo(title);
+        assertThat(posts.getContent()).isEqualTo(content); // 검증
+    }
+
+    /*@Test
+    public void BaseTimeEntity_등록() {
+        //given
+        LocalDateTime now = LocalDateTime.of(2019, 6, 4, 0, 0, 0);
+        postsRepository.save(Posts.builder()
+                .title("title")
+                .content("content")
+                .author("author")
+                .build());
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+
+        System.out.println(">>>>>>>>> createDate=" + posts.getCreatedDate() + ", modifiedDate=" + posts.getModifiedDate());
+
+        assertThat(posts.getCreatedDate()).isAfter(now);
+        assertThat(posts.getModifiedDate()).isAfter(now);
+    }*/
+}
